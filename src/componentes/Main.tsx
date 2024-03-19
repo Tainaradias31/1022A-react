c9cfbb50c3d3deeb23f87fb0b1469ffcb2520e58
Tainaradias31 <116743198+Tainaradias31@users.noreply.github.com>
2024-03-19 10:14:30 -0400

    Box estudantes na main.tsx

--- src/componentes/Main.tsx
+++ src/componentes/Main.tsx
@@ -1,9 +1,17 @@
+import "./Main.css"
+
 function Main(){
     return(
-        <>
-        <main> Main </main>
-        <h1> Meu H1</h1>
-        </>
+        <main>
+            <div className="estudante-box">
+                <img className="estudante-foto" src="https://github.com/Tainaradias31.png" alt=""/>
+                <div className="informações"> 
+                  <h3 className="estudante_nome">Tainarazinha</h3>
+                  <p className="estudante_descricao">Papa anjo rs</p>
+                  <a className="link-git" href="https://github.com/Tainaradias31">GitHub</a>
+                </div>
+            </div>
+        </main>
     )
 }
-export default Main;
+export default Main; 
